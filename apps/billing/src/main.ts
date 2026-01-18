c4680dc9367fa58dc9a771b7e7e619ea59634fec
eodaneze <ezealiokechukwu1999@gmail.com>
2026-01-18 15:58:40 +0100

    billing service setup

--- apps/billing/src/main.ts
+++ apps/billing/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { BillingModule } from './billing.module';
+import { RmqService } from '@app/common/rmq/rmq.service';
+import { application } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(BillingModule);
-  await app.listen(process.env.port ?? 3000);
+
+  const rmqService = app.get<RmqService>(RmqService);
+
+  app.connectMicroservice(rmqService.getOptions('BILLING'));
+  // await app.listen(process.env.port ?? 3000);
+  await app.startAllMicroservices();
 }
 bootstrap();
