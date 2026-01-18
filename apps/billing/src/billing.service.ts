c786d3f2ea38e77f44b60d38cc4d44a63cfc9329
eodaneze <ezealiokechukwu1999@gmail.com>
2026-01-18 12:09:38 +0100

    created the different apps

--- apps/billing/src/billing.service.ts
+++ apps/billing/src/billing.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
+export class BillingService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World! this is  the billing service';
   }
 }
